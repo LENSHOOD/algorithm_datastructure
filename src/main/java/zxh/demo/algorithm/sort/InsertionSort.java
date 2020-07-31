0456c653c6c6998e4fcdf4d1322454ebdfcb5ed1
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-07-31 23:23:51 +0800

    refactor: revert to move method at insertion sort

--- src/main/java/zxh/demo/algorithm/sort/InsertionSort.java
+++ src/main/java/zxh/demo/algorithm/sort/InsertionSort.java
@@ -16,15 +16,12 @@ public class InsertionSort<T extends Comparable<T>> implements Sortable {
     public void sort() {
         for (int i = 1; i < collection.size(); i++) {
             T valueToBeCompared = collection.get(i);
-            int j = i - 1;
-            for (; j >= 0; j--) {
-                if (valueToBeCompared.compareTo(collection.get(j)) >= 0) {
+            for (int j = i - 1; j >= -1; j--) {
+                if (j == -1 || valueToBeCompared.compareTo(collection.get(j)) >= 0) {
+                    collection.move(j + 1, i, 1);
                     break;
                 }
-
-                collection.set(j + 1, collection.get(j));
             }
-            collection.set(j + 1, valueToBeCompared);
         }
     }
 }
