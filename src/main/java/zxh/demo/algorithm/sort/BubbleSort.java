56ed01dde181dde308f817a08f184cad920d5fc4
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-08-01 17:36:44 +0800

    fix: wrong buble sort implementation

--- src/main/java/zxh/demo/algorithm/sort/BubbleSort.java
+++ src/main/java/zxh/demo/algorithm/sort/BubbleSort.java
@@ -16,8 +16,8 @@ public class BubbleSort<T extends Comparable<T>> implements Sortable {
     public void sort() {
         for (int i = 0; i < collection.size(); i++) {
             boolean swapFlag = false;
-            for (int j = i + 1; j < collection.size(); j++) {
-                swapFlag = collection.compareAndSwap(i, j) || swapFlag;
+            for (int j = 0; j < collection.size() - i - 1 ; j++) {
+                swapFlag = collection.compareAndSwap(j, j + 1) || swapFlag;
             }
             if (!swapFlag) {
                 break;
